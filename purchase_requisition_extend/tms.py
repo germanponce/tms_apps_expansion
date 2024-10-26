dad557b79d783cdbec65e968d314be171ef599fb
Germán Ponce Dominguez <german.ponce@argil.mx>
2024-10-25 23:43:43 -0600

    Actualizaciones

--- purchase_requisition_extend/tms.py
+++ purchase_requisition_extend/tms.py
@@ -133,6 +133,38 @@ class AccountInvoice(models.Model):
                         move_line.write({'store_id': invoice_line.x_store_id.id})
         return res
 
+    # Load all unsold PO lines
+    @api.onchange('purchase_id')
+    def purchase_order_change(self):
+        if not self.purchase_id:
+            return {}
+        if not self.partner_id:
+            self.partner_id = self.purchase_id.partner_id.id
+
+        vendor_ref = self.purchase_id.partner_ref
+        if vendor_ref and (not self.reference or (
+                vendor_ref + ", " not in self.reference and not self.reference.endswith(vendor_ref))):
+            self.reference = ", ".join([self.reference, vendor_ref]) if self.reference else vendor_ref
+
+        if not self.invoice_line_ids:
+            #as there's no invoice line yet, we keep the currency of the PO
+            self.currency_id = self.purchase_id.currency_id
+
+        new_lines = self.env['account.invoice.line']
+        for line in self.purchase_id.order_line - self.invoice_line_ids.mapped('purchase_line_id'):
+            data = self._prepare_invoice_line_from_po_line(line)
+            if line.purchase_line_id and line.purchase_line_id.store_id:
+                # Pasar el valor de store_id de la línea de compra a x_store_id en la línea de factura
+                data['x_store_id'] = line.purchase_line_id.store_id.id
+            new_line = new_lines.new(data)
+            new_line._set_additional_fields(self)
+            new_lines += new_line
+
+        self.invoice_line_ids += new_lines
+        self.payment_term_id = self.purchase_id.payment_term_id
+        self.env.context = dict(self.env.context, from_purchase_order_change=True)
+        self.purchase_id = False
+        return {}
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
@@ -195,7 +227,6 @@ class PurchaseOrder(models.Model):
                     line.x_store_id = line.purchase_line_id.store_id.id
         result['context']['default_origin'] = self.name
         result['context']['default_reference'] = self.partner_ref
-        print ("#************ result: ", result)
         return result
 
 class PurchaseOrderLine(models.Model):
